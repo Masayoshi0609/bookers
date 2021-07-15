4b45f23a7ac339a78bb8105fc851368cd98b1196
Masayoshi0609 <masayoshi9876@gmail.com>
2021-07-15 11:05:28 +0000

    [Finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id), flash: { created: 'Book was succesfully created.' }
+      redirect_to book_path(@book.id), flash: { created: 'Book was successfully created.' }
     else
       @books = Book.all
       render :index
@@ -26,9 +26,9 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), flash: { updated: 'Book was succesfully updated.' }
+      redirect_to book_path(@book), flash: { updated: 'Book was successfully updated.' }
     else
-      render :show
+      render :edit
     end
   end
 
